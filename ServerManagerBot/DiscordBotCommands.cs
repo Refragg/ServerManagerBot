ebde0ac1417021e9d0a5c8a15dfd4b518fdc6124
Refrag <R3FR4GYT@gmail.com>
2024-07-10 17:50:06 +0200

    Make the Discord bot send responses to interactions
    This just adds a basic response to the command interactions so that Discord doesn't display the action as a failure.

--- ServerManagerBot/DiscordBotCommands.cs
+++ ServerManagerBot/DiscordBotCommands.cs
@@ -1,4 +1,5 @@
 using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.SlashCommands.Attributes;
 
@@ -16,12 +17,20 @@ public class DiscordBotCommands
         [SlashCommand("stop", "Stops the server from running")]
         public async Task Stop(InteractionContext ctx)
         {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder()
+                    .WithContent("Stopping the server..."));
+            
             Task.Run(() => Program.Stop());
         }
         
         [SlashCommand("quit", "Quits the server manager")]
         public async Task Quit(InteractionContext ctx)
         {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder()
+                    .WithContent("Quiting the server manager..."));
+            
             Task.Run(() => Program.Shutdown());
         }
         
@@ -29,6 +38,10 @@ public class DiscordBotCommands
         public async Task Send(InteractionContext ctx,
             [Option("Command", "The command to send to the server")] string command)
         {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder()
+                    .WithContent("Sending the input to the process..."));
+            
             Task.Run(() => DiscordBot.SendCommand(command));
         }
     }
